808113e4d2e32879b196505668b07de33c8ef54b
Amine_ <abdelamine.mehdaoui@gmail.com>
2019-10-21 03:00:41 +0200

    Package challenge (create a JAR with a existant package and use it in other classe)

---
+++ SeriesPackageChallengeTest/src/com/packageTest/Main.java
@@ -0,0 +1,13 @@
+package com.packageTest;
+
+import com.challengePackage.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+	for(int i=0; i<= 10;i++)
+        {
+            System.out.println(Series.nSum(i));
+        }
+    }
+}
