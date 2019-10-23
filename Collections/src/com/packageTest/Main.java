5290ebb109052e9dde199e3668795f5fb6d28ed4
Amine_ <abdelamine.mehdaoui@gmail.com>
2019-10-23 17:22:54 +0200

    4 Collections comparator and comparable

--- Collections/src/com/packageTest/Main.java
+++ Collections/src/com/packageTest/Main.java
@@ -1,6 +1,7 @@
 package com.packageTest;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,41 +9,32 @@ public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Béjaia", 8, 10);
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-        printList(seatCopy);
-        seatCopy.get(1).reserve();
+
         if(theatre.reserveSeat("A02"))
         {
             System.out.println("Please pay A02");
         }else{
             System.out.println("Seat is already reserved!");
         }
-        Theatre.Seat minSet = Collections.min(seatCopy);
-        Theatre.Seat maxSet = Collections.max(seatCopy);
-        System.out.println("Min set number is " + minSet.getSeatNumber());
-        System.out.println("Mxs set number is " + maxSet.getSeatNumber());
+        List<Theatre.Seat> reverseSeat = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeat);
+        printList(reverseSeat);
+
+        List<Theatre.Seat> priceSeat = new ArrayList<>(theatre.getSeats());
+        priceSeat.add(theatre.new Seat("BOO", 13));
+        priceSeat.add(theatre.new Seat("A00", 13));
+        Collections.sort(priceSeat,theatre.PRICE_ORDER);
+        printList(priceSeat);
     }
 
     public static void printList(List<Theatre.Seat> list)
     {
         for(Theatre.Seat seat: list)
         {
-            System.out.println(" " + seat.getSeatNumber());
+            System.out.println(" " + seat.getSeatNumber() + " " + seat.getPrice());
         }
         System.out.println();
         System.out.printf("=======================================");
     }
-    public static void sortedList(List< ? extends Theatre.Seat> list)
-    {
-        for( int i=0; i<list.size()-1; i++)
-        {
-            for(int j = i+1; j<list.size(); j++)
-            {
-                if(list.get(i).compareTo(list.get(j)) >0 )
-                {
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
+
 }
--- Collections/src/com/packageTest/Theatre.java
+++ Collections/src/com/packageTest/Theatre.java
@@ -6,10 +6,46 @@ import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    public  ArrayList<Seat> seats = new ArrayList<>();
-    // On utilisant HashSet les seats retourné seront pas ordonnée
+    private   ArrayList<Seat> seats = new ArrayList<>();
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            if(seat1.getPrice() < seat2.getPrice())
+            {
+                return -1;
+            }else if(seat1.getPrice() > seat2.getPrice())
+                {
+                    return 1;
+                }else
+                    {
+                        return 0;
+                    }
 
-        public Theatre(String theatreName, int numRows, int seatsPerRows )
+        }
+    };
+
+    /**
+     * getter theatreName variable
+     * @return
+     */
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    /***
+     * for testing Getter seats array
+     */
+    public Collection<Seat> getSeats() {
+        return seats;
+    }
+
+    /**
+     * Constuctor
+     * @param theatreName
+     * @param numRows
+     * @param seatsPerRows
+     */
+    public Theatre(String theatreName, int numRows, int seatsPerRows )
         {
             this.theatreName = theatreName;
             int lastRow = 'A' + (numRows -1); // Le -1 parceque on a déja le premier rang qui est le A
@@ -17,50 +53,74 @@ public class Theatre {
                 {
                     for(int seatNum =0; seatNum<=seatsPerRows; seatNum++)
                     {
-                        Seat seat = new Seat(row + String.format("%02d", seatNum));
+                        double price = 12.0;
+                        if((row < 'D') && (seatNum >= 4) && (seatNum <= 9))
+                        {
+                            price = 14.0;
+                        }else if((row > 'F') || (seatNum < 4) || (seatNum > 9))
+                        {
+                            price = 7.00;
+                        }
+                        Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                          seats.add(seat);
                     }
                 }
         }
 
-    public String getTheatreName() {
-        return theatreName;
-    }
 
 
-    public boolean reserveSeat(String seatNumber)
-    {
-        Seat requestedSeat = new Seat(seatNumber);
-        int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
-        if(foundSeat >= 0)
-        {
-            return seats.get(foundSeat).reserve();
-        }else
-            {
-                System.out.println("There is no seat " + seatNumber);
-                return false;
-            }
-    }
+    public   boolean reserveSeat(String seatNumber)
+                {
+                    Seat requestedSeat = new Seat(seatNumber, 0);
+                    int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
+                        if(foundSeat >= 0)
+                        {
+                            return seats.get(foundSeat).reserve();
+                        }else
+                            {
+                                System.out.println("There is no seat " + seatNumber);
+                                return false;
+                            }
+                }
 
-    //for testing
-    public void getSeats()
-    {
-        for(Seat seat: seats)
-        {
-            System.out.println(seat.getSeatNumber()) ;
-        }
-    }
 
     //************************************************************
-    // la declaration d'une inner class Seat
+
+    /**
+     * la declaration d'une inner class Seat
+     */
+
 
     public class  Seat implements Comparable<Seat>
     {
         private final String seatNumber;
+        private double price;
         private boolean reserved = false;
 
-        public Seat(String seatNumber) {
+        /**
+         * Getters
+         * @return
+         */
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        /**
+         *Getters End
+         */
+
+        /**
+         * Constructor
+         * @param seatNumber
+         * @param price
+         */
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
         }
 
         @Override
@@ -68,9 +128,7 @@ public class Theatre {
             return this.seatNumber.compareToIgnoreCase(seat.seatNumber);
         }
 
-        public String getSeatNumber() {
-            return seatNumber;
-        }
+
 
         public boolean reserve()
         {
